421fca4167557abb50e46c5820d0a5240a55a752
sruthybabu <sruthybabu64@gmail.com>
2022-05-19 00:18:33 +0530

    Employee search complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addemployee from './components/Addemployee';
+import Searchemployee from './components/Searchemployee';
 function App() {
   return (
     <div>
 
 <Addemployee/>
+<Searchemployee/>
 
     </div>
   );
